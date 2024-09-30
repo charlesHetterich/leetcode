5a15d1be0a98fef72f2ac6fb70b286d38866d441
Charles Hetterich <hetterich.charles@gmail.com>
2024-09-29 21:16:47 -0500

    unfinished find median... need to prep for IBM tm

---
+++ median_sorted_arrays/src/main.rs
@@ -0,0 +1,15 @@
+struct Solution;
+impl Solution {
+    pub fn find_median_sorted_arrays(nums1: Vec<i32>, nums2: Vec<i32>) -> f64 {
+        0.0
+    }
+}
+fn main() {
+    println!(
+        "{:?}",
+        Solution::find_median_sorted_arrays(
+            vec![1, 2, 2, 9, 11, 15, 15, 15, 15],
+            vec![1, 2, 3, 8, 9, 10]
+        )
+    );
+}
